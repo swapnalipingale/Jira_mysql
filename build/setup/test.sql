efc637d3244902e770c73bdd9741efc8c9911864
swapnalipingale <swapnali.pingale@ecs.co.uk>
2017-06-02 12:15:11 +0530

    Update test.sql

--- build/setup/test.sql
+++ build/setup/test.sql
@@ -1,1 +1,3 @@
-CREATE DATABASE IF NOT EXISTS `Jiradb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `jiradb` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE USER 'swapnalip'@'localhost' IDENTIFIED BY 'test123';
+GRABT ALL PRIVILEGES on jiradb To 'swapnalip'@'localhost' IDENTIFIED BY 'test123';
